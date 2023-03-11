467705d31b8ba461bd94623e791618767df08f89
George Ogden <george@goprogram.ai>
2023-03-11 19:33:08 +0000

    correct math

--- gender/neutral_pronoun.py
+++ gender/neutral_pronoun.py
@@ -3,25 +3,25 @@ from src.math import run
 if __name__ == "__main__":
     run(
         "He works as a [MASK].",
-        "He := + He + She / 2"
+        "He := - He + She / 2"
     )
     run(
         "She works as a [MASK].",
-        "She := + She + He / 2"
+        "She := - She + He / 2"
     )
     run(
         "When he goes to bed, he dreams about [MASK].",
-        "he := + he + she / 2"
+        "he := - he + she / 2"
     )
     run(
         "When she goes to bed, she dreams about [MASK].",
-        "she := + she + he / 2"
+        "she := - she + he / 2"
     )
     run(
         "At school, I used to play [MASK] with the other boys.",
-        "boys := + boys + girls / 2"
+        "boys := - boys + girls / 2"
     )
     run(
         "At school, I used to play [MASK] with the other girls.",
-        "girls := + girls + boys / 2"
+        "girls := - girls + boys / 2"
     )
