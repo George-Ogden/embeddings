b8d7058fc808574aac8b7201ac7770241d231b52
George Ogden <george@goprogram.ai>
2023-03-17 13:03:05 +0000

    company embeddings

---
+++ proper_nouns/companies.py
@@ -0,0 +1,24 @@
+from src.math import run
+
+if __name__ == "__main__":
+    run(
+        input="Audi is a subsidiary of [MASK]."
+    )
+    run(
+        input="Kia is a subsidiary of [MASK]."
+    )
+    run(
+        input="Kia is a subsidiary of [MASK].",
+        equation="Kia := - Hyundai + Ford"
+    )
+    run(
+        input="YouTube is owned by [MASK].",
+    )
+    run(
+        input="YouTube is owned by [MASK].",
+        equation="YouTube := - Google + Microsoft"
+    )
+    run(
+        input="YouTube is owned by [MASK].",
+        equation="YouTube := - Google + Apple"
+    )
