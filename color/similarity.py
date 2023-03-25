3382957a51f9b8ae165b1b3bae4ecf0a82b99e28
George Ogden <george@goprogram.ai>
2023-03-25 08:16:43 +0000

    use updated plotting methods

--- color/similarity.py
+++ color/similarity.py
@@ -1,4 +1,6 @@
-from src.similarity import plot_file
+from src.similarity import plot_file as plot_file_similarity
+from src.plot import plot_file as plot_file_embeddings
 
 if __name__ == "__main__":
-    plot_file("data/colors.txt")
+    plot_file_similarity("data/colors.txt")
+    plot_file_embeddings("data/colors.txt")
