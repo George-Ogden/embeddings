1eebbeb43317595c7954e7031ae058f97e3af03e
George Ogden <george@goprogram.ai>
2023-04-01 08:51:57 +0100

    plot country similarities

---
+++ examples/countries/similarity.py
@@ -0,0 +1,6 @@
+from src.similarity import plot_file as plot_file_similarity
+from src.plot import plot_file as plot_file_embeddings
+
+if __name__ == "__main__":
+    plot_file_similarity("data/countries.txt")
+    plot_file_embeddings("data/countries.txt")
